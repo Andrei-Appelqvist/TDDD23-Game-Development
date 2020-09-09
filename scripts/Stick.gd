04bece6ba1395b7ae374572643a95d1e3c22fdb5
anna <annli504@student.liu.se>
2020-09-09 16:14:09 +0200

    added backup folder for svg files

--- scripts/Stick.gd
+++ scripts/Stick.gd
@@ -9,10 +9,10 @@ var Mouse_position
 
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
-	Mouse_position = get_local_mouse_position()
-	rotation += Mouse_position.angle()
-	print("asf")
-	pass # Replace with function body.
+    Mouse_position = get_local_mouse_position()
+    rotation += Mouse_position.angle()
+    print("asf")
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
