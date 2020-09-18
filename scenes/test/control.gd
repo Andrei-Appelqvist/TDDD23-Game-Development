1a27309ddf276af3822e798f2ed81f62c72685e8
Eric Jönsson <eric.joensson.sarv@gmail.com>
2020-09-19 00:26:02 +0200

    new test worm settings

--- scenes/test/control.gd
+++ scenes/test/control.gd
@@ -1,9 +1,19 @@
 extends RigidBody2D
 var desired_rotation = 0
-    
+
+func _ready():
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _input(event):
     if event is InputEventMouseMotion:
         desired_rotation += event.relative.x
+        
+
 func _integrate_forces(state):
+    if abs(desired_rotation) > 10:
+        var negation = 1
+        if desired_rotation < 0:
+            negation = -1
+        desired_rotation = 10 * negation
     state.angular_velocity = desired_rotation
     desired_rotation = 0
