c0f14e81941610ef19ed91dc498481c97661b465
Andrei <andmo435@student.liu.se>
2020-09-23 15:55:32 +0200

    let there be light

--- scenes/player/seg.gd
+++ scenes/player/seg.gd
@@ -8,14 +8,14 @@ var ROTATION_SPEED = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 func _integrate_forces(state):
-    var mult = 0.5
-    if (abs(self.rotation_degrees) > 30):
-        mult = 5
-        #angular_velocity -= ROTATION_SPEED * mult * rotation_degrees/360
-    if (abs(self.rotation_degrees) < 3):
-        mult = 0
-    #else:
-        #angular_velocity -= ROTATION_SPEED * mult * rotation_degrees/360
+	var mult = 0.5
+	if (abs(self.rotation_degrees) > 30):
+		mult = 5
+		#angular_velocity -= ROTATION_SPEED * mult * rotation_degrees/360
+	if (abs(self.rotation_degrees) < 3):
+		mult = 0
+	#else:
+		#angular_velocity -= ROTATION_SPEED * mult * rotation_degrees/360
