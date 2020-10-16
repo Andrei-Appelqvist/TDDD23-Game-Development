b6c7268de4a88ffe1305779e3ee3d9d6724f642e
Andrei Moga <andrei.moga@live.se>
2020-10-16 13:27:53 +0200

    nya sprites till wonderland och avslut 1

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,17 +2,17 @@ extends Node2D
 
 var velocity = Vector2()
 func set_start_speed(velocity):
-    $control.apply_impulse(Vector2(),velocity)
+	$control.apply_impulse(Vector2(),velocity)
 
 func save():
-    var save_dict = {
-        "filename" : get_filename(),
-        "parent" : get_parent().get_path(),
-        "pos_x" : $control.global_position.x,
-        "pos_y" : $control.global_position.y,
-        "rotation": $control.global_rotation_degrees,
-        "force_x": velocity.x,
-        "force_y": velocity.y,
-    }
-    print(save_dict)
-    return save_dict
+	var save_dict = {
+		"filename" : get_filename(),
+		"parent" : get_parent().get_path(),
+		"pos_x" : $control.global_position.x,
+		"pos_y" : $control.global_position.y,
+		"rotation": $control.global_rotation_degrees,
+		"force_x": velocity.x,
+		"force_y": velocity.y,
+	}
+	print(save_dict)
+	return save_dict
