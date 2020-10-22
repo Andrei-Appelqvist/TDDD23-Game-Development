acf6101221a2ae48f406f31fe9a4f0ca981e49ef
Eric Jönsson <eric.joensson.sarv@gmail.com>
2020-10-22 14:10:40 +0200

    blurgh

--- scenes/player/control.gd
+++ scenes/player/control.gd
@@ -3,19 +3,19 @@ var desired_rotation = 0
 var frozen = false
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
-	if event is InputEventMouseMotion and frozen == false:
-		desired_rotation += event.relative.x
-		
+    if event is InputEventMouseMotion and frozen == false:
+        desired_rotation += event.relative.x
+        
 
 func _integrate_forces(state):
-	get_parent().velocity = state.get_linear_velocity()
-	if abs(desired_rotation) > 10:
-		var negation = 1
-		if desired_rotation < 0:
-			negation = -1
-		desired_rotation = 8 * negation
-	state.angular_velocity = desired_rotation
-	desired_rotation = 0
+    get_parent().velocity = state.get_linear_velocity()
+    if abs(desired_rotation) > 10:
+        var negation = 1
+        if desired_rotation < 0:
+            negation = -1
+        desired_rotation = 8 * negation
+    state.angular_velocity = desired_rotation
+    desired_rotation = 0
