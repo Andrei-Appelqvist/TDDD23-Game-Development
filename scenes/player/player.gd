60841cd4559ac329c2f5233bd12782b999f2e212
Eric Jönsson <eric.joensson.sarv@gmail.com>
2020-10-22 14:23:54 +0200

    performance

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -12,22 +12,22 @@ func _ready():
     pass
     
 func save():
-	var save_dict = {
-		"filename" : get_filename(),
-		"parent" : get_parent().get_path(),
-		"pos_x" : $control.global_position.x,
-		"pos_y" : $control.global_position.y,
-		"rotation": $control.global_rotation_degrees,
-		"force_x": velocity.x,
-		"force_y": velocity.y,
-		"music" : current_area,
-	}
-	print(save_dict)
-	return save_dict
+    var save_dict = {
+        "filename" : get_filename(),
+        "parent" : get_parent().get_path(),
+        "pos_x" : $control.global_position.x,
+        "pos_y" : $control.global_position.y,
+        "rotation": $control.global_rotation_degrees,
+        "force_x": velocity.x,
+        "force_y": velocity.y,
+        "music" : current_area,
+    }
+    print(save_dict)
+    return save_dict
 
 func _process(delta):
-	if follow_birb == true:
-		if camera_done == false:
-			get_parent().get_node("END-CAMERA").position = $control/Camera2D.global_position
-			camera_done = true
-		position = get_parent().get_node("Birb").global_position + Vector2(-1000, -400)
+    if follow_birb == true:
+        if camera_done == false:
+            get_parent().get_node("END-CAMERA").position = $control/Camera2D.global_position
+            camera_done = true
+        position = get_parent().get_node("Birb").global_position + Vector2(-1000, -400)
