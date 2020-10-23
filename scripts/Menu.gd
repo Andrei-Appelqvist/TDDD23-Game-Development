0619bd49ba6dc7d4b0f8e90a949bbcf25117c4e1
Anna Lindqvist <annli504@student.liu.se>
2020-10-23 16:26:08 +0200

    background fix to hell

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -2,38 +2,38 @@ extends MarginContainer
 var save_exists = true
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	var save_game = File.new()
-	if not save_game.file_exists("user://savegame.save"):
-		save_exists = false
-		
-		
-	
+    Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+    var save_game = File.new()
+    if not save_game.file_exists("user://savegame.save"):
+        save_exists = false
+        
+        
+    
 func _on_Continue_gui_signal(event):
-	if event.is_action_pressed("jump") and save_exists:
-		get_node("HBoxContainer/VBoxContainer/Continue").add_color_override("Red", Color.red)
-		get_tree().change_scene("res://scenes/Main.tscn")
+    if event.is_action_pressed("jump") and save_exists:
+        get_node("HBoxContainer/VBoxContainer/Continue").add_color_override("Red", Color.red)
+        get_tree().change_scene("res://scenes/Main.tscn")
 
 
 func NewGame_gui_input(event):
-	if event.is_action_pressed("jump"):
-		if save_exists:
-			$AcceptDialog.show()
-		else:
-			get_tree().change_scene("res://scenes/newgame_cutscene.tscn")
-	pass # Replace with function body.
+    if event.is_action_pressed("jump"):
+        if save_exists:
+            $AcceptDialog.show()
+        else:
+            get_tree().change_scene("res://scenes/newgame_cutscene.tscn")
+    pass # Replace with function body.
 
 
 func Quit_gui_input(event):
-	if event.is_action_pressed("jump"):
-		get_tree().quit()
+    if event.is_action_pressed("jump"):
+        get_tree().quit()
 
 
 func Credits_gui_input(event):
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 func AcceptDialog_confirmed():
-	var save_dir = Directory.new()
-	save_dir.remove("user://savegame.save")
-	get_tree().change_scene("res://scenes/newgame_cutscene.tscn")
+    var save_dir = Directory.new()
+    save_dir.remove("user://savegame.save")
+    get_tree().change_scene("res://scenes/newgame_cutscene.tscn")
