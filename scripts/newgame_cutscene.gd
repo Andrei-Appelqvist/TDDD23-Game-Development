3a0352d83c0febe661dad13ec28f4018285b94c8
Eric Jönsson <eric.joensson.sarv@gmail.com>
2020-10-25 14:44:16 +0100

    fixed dependency error and remove sfx

--- scripts/newgame_cutscene.gd
+++ scripts/newgame_cutscene.gd
@@ -1,6 +1,7 @@
 extends VideoPlayer
     
 func _ready():
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
     $"black-rect".modulate.a = 1
     $"black-rect".fade(1,1)
     
@@ -15,6 +16,8 @@ func _on_VideoPlayer_finished():
     get_tree().change_scene("res://scenes/Main.tscn")
 
 func blackrect_fade_complete():
+    print('complete')
     if $"black-rect".modulate.a == 1:
+        print('full')
         yield(get_tree().create_timer(1.0), "timeout")
         get_tree().change_scene("res://scenes/Main.tscn")  
